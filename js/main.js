fd6ed7f826ad4fc8fe0760e0c1a534f33c450011
Fred - Antigravity <infosilenceondanse@gmail.com>
2026-02-25 15:34:20 -0500

    Fix form POST by using raw FormData and removing headers

--- js/main.js
+++ js/main.js
@@ -197,8 +197,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             fetch("/", {
                 method: "POST",
-                headers: { "Content-Type": "application/x-www-form-urlencoded" },
-                body: new URLSearchParams(formData).toString(),
+                body: formData,
             })
                 .then(response => {
                     if (response.ok) {
